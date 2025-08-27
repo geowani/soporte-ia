74eb27970eb79dd58499382aa5f6269c4ae6810e
Lester <lestradae6@miumg.edu.gt>
2025-08-26 22:28:03 -0600

    Cuadro administrador tamaño

--- app/src/pages/AdminDashboard.jsx
+++ app/src/pages/AdminDashboard.jsx
@@ -30,7 +30,7 @@ export default function AdminDashboard({ onLogout }) {
 
       {/* Contenedor central */}
       <div className="min-h-screen grid place-items-center p-6">
-        <section className="w-full max-w-3xl rounded-2xl border border-white/20 p-10 md:p-12 shadow-[0_20px_60px_rgba(0,0,0,.45)] bg-white/90 text-black">
+        <section className="w-full max-w-3xl rounded-2xl border border-white/20 p-12 md:p-16 shadow-[0_20px_60px_rgba(0,0,0,.45)] bg-white/90 text-black">
           {/* Header */}
           <div className="flex justify-between items-center mb-8">
             <h1 className="text-3xl font-extrabold">Administrador</h1>
