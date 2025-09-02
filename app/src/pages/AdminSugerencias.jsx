afd5633e000c7ffbb04b5b9731385573018f5eb3
Lester <lestradae6@miumg.edu.gt>
2025-09-01 20:02:40 -0600

    boton de regresar negro

--- app/src/pages/AdminSugerencias.jsx
+++ app/src/pages/AdminSugerencias.jsx
@@ -56,7 +56,7 @@ export default function AdminSugerencias() {
             </h1>
             <button
               onClick={() => nav("/admin")}
-              cclassName="px-5 py-2 rounded-full bg-red-500/90 hover:bg-red-600 font-semibold shadow-md transition focus:outline-none focus:ring-2 focus:ring-white/50"
+              className="px-5 py-2 rounded-full bg-red-500/90 hover:bg-red-600 font-semibold shadow-md transition focus:outline-none focus:ring-2 focus:ring-white/50"
             >
               Regresar
             </button>
