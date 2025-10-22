b89120ed076bcd999e2f27e50687c3521b5fa719
Lester <lestradae6@miumg.edu.gt>
2025-10-21 21:37:37 -0600

    cambiando texto en boton dashboard

--- app/src/pages/Dashboard.jsx
+++ app/src/pages/Dashboard.jsx
@@ -102,7 +102,7 @@ export default function Dashboard({ onLogout }) {
         onClick={handleLogout}
         className="absolute right-6 top-6 px-5 py-2 rounded-full bg-red-500/90 hover:bg-red-600 font-semibold shadow-md transition focus:outline-none focus:ring-2 focus:ring-white/50"
       >
-        Salir
+        Cerrar Sesión
       </button>
 
       {/* Contenido */}
