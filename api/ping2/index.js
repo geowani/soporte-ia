e820db147a57900c9e84c3fc6ee80979643fb4a4
Lester <lestradae6@miumg.edu.gt>
2025-11-13 19:09:48 -0600

    verificando respuesta del ping

--- api/ping2/index.js
+++ api/ping2/index.js
@@ -1,8 +1,6 @@
-// CommonJS (sin "export default")
 module.exports = async function (context, req) {
   context.res = {
     status: 200,
-    // Para Functions v4 puedes usar jsonBody, pero body también funciona:
     body: { ok: true, env: Object.keys(process.env).length }
   };
 };
